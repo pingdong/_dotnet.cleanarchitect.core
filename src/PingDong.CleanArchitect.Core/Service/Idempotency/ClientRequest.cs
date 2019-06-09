82a6d1bfb574e80d23dfcbbde647cc416107423d
Ping Dong <ping.dong@live.com>
2019-06-09 18:25:13 +1200

    Update ClientRequest

--- src/PingDong.CleanArchitect.Core/Service/Idempotency/ClientRequest.cs
+++ src/PingDong.CleanArchitect.Core/Service/Idempotency/ClientRequest.cs
@@ -3,7 +3,7 @@ using PingDong.CleanArchitect.Core;
 
 namespace PingDong.CleanArchitect.Service
 {
-    public class ClientRequest : Entity<Guid>
+    public class ClientRequest : Entity<Guid>, IAggregateRoot
     {
         public ClientRequest(string name, DateTime time)
         {
