3a2395f005f82ae14ce3dea4b0c15942fc80d279
Ping Dong <ping.dong@live.com>
2019-07-03 22:01:37 +1200

    Adding Domain Exception

---
+++ src/PingDong.CleanArchitect.Core/Exception/DomainException.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace PingDong.CleanArchitect.Core
+{
+    /// <summary>
+    /// Exception type for domain exceptions
+    /// </summary>
+    public class EventDomainException : Exception
+    {
+        public EventDomainException() : base()
+        { }
+
+        public EventDomainException(string message)
+            : base(message)
+        { }
+
+        public EventDomainException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
