df43fb007c0cb777be4dea6a8f630c309f6df0a7
Ping Dong <ping.dong@live.com>
2019-07-16 21:23:44 +1200

    Added Command and DomainEvent

---
+++ src/PingDong.CleanArchitect.Core.UnitTests/Domain/DomainEvent.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace PingDong.CleanArchitect.Core.UnitTests
+{
+    public class DomainEventTest
+    {
+        [Fact]
+        public void DomainEvent_EmptyCtor()
+        {
+            var evt = new DomainEvent();
+
+            Assert.Null(evt.CorrelationId);
+        }
+
+        [Fact]
+        public void DomainEvent_WithCorrelationId()
+        {
+            var evt = new DomainEvent("Test");
+
+            Assert.Equal("Test", evt.CorrelationId);
+        }
+
+        [Fact]
+        public void DomainEvent_Property_CorrelationId()
+        {
+            var evt = new DomainEvent
+            {
+                CorrelationId = "Test"
+            };
+
+            Assert.Equal("Test", evt.CorrelationId);
+        }
+    }
+}
